6cab43fdbac405bdaa676c55cc2295b8f9db9de9
jtheisin <117414620+jtheisin@users.noreply.github.com>
2023-09-18 18:08:29 -0500

    Create hw2.py

---
+++ homework/hw2/hw2.py
@@ -0,0 +1,26 @@
+
+usr_input = input("Please enter a string.")
+
+def is_palyndrom(string):
+    if len(string)==0:
+        return ' IS '
+    else:
+        if string[0].isalpha():
+            if string[-1].isalpha():
+                if string[0].lower() == string[-1].lower():
+                    return is_palyndrom(string[1:-1])
+                else:
+                    return ' is not '
+            else:
+                return is_palyndrom(string[:-1])
+        else:
+            return is_palyndrom(string[1:])
+        
+def reverse(string):
+    if len(string)==0:
+        return string
+    else:
+        return reverse(string[1:])  + string[0]
+
+print('The reverse of the string provided: ', reverse(usr_input))
+print('Your string' + is_palyndrom(usr_input) + 'a palyndrom.')
